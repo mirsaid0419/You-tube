bf1e036bf3f897b0dc73781ead8b8d92063b0947
Toxirov36 <dilshodbektohirov40@gmail.com>
2026-03-12 14:14:46 +0500

    Add Users

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString } from "class-validator";
+
+export class CreateUserDto {
+    @ApiProperty()
+    @IsEmail()
+    email: string
+
+    @ApiProperty()
+    @IsString()
+    username: string
+
+    @ApiProperty()
+    @IsString()
+    firstName: string
+
+    @ApiProperty()
+    @IsString()
+    lastName: string
+
+    @ApiProperty()
+    @IsString()
+    avatar?:string
+}
